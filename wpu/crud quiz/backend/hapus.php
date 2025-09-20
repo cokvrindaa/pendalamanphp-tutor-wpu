07c946a6e6090e3dad9d485dfb9255bbddeea233
master.cokorda@gmail.com <master.cokorda@gmail.com>
2025-09-20 17:23:28 +0800

    hapus gambar - php quizz

--- wpu/crud quiz/backend/hapus.php
+++ wpu/crud quiz/backend/hapus.php
@@ -1,12 +1,21 @@
 <?php  
 require_once '../koneksi.php';
 $id = $_GET['id'];
+
+// Ambil nama file gambar dulu
+$queryGambar = mysqli_query($koneksi, "SELECT gambar FROM stok WHERE id = '$id'");
+$dataGambar = mysqli_fetch_assoc($queryGambar);
+
+// Hapus data dari database
 $sqlsintaks = "DELETE FROM stok WHERE id = '$id'";
 $eksekusi = mysqli_query($koneksi, $sqlsintaks);
-$sqlgambar = "SELECT gambar FROM stok WHERE id = '$id'";
 
-// Pengecekan eror
+// Kalau data berhasil dihapus, hapus juga file gambar
 if (mysqli_affected_rows($koneksi) > 0) {
+    if ($dataGambar && file_exists("../img/" . $dataGambar['gambar'])) {
+        unlink("../img/" . $dataGambar['gambar']);
+    }
+
     echo "
     <script>
         alert('Data Berhasil dihapus');
@@ -14,6 +23,6 @@ if (mysqli_affected_rows($koneksi) > 0) {
     </script>
     ";
 } else {
-    echo "Data gagal dihapus". mysqli_errno($koneksi);
+    echo "Data gagal dihapus. Error: " . mysqli_error($koneksi);
 }
 ?>
